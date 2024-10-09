cd8b251cea11c5da446511c73c6dc7c44ecfac1e
Fuzail Hassan Siddiqui <fuzail.hassan2@gmail.com>
2024-10-09 20:57:47 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cyberpunk"],
+  },
+}
+
